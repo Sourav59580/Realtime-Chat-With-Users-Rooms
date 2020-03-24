fbb01bc772ceda46db6cf4be746dade1c131fbd0
sourav santra <souravsantra59580@gmail.com>
2020-03-25 00:56:33 +0530

    second commit(24/03/2020)

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require("express");
-
+const path = require("path");
+const socketio = require("socket.io");
 
 
 const app = express();
 
+//set static file
+app.use(express.static(path.join(__dirname,"public")));
+
+
+//Routes
+app.get("/",(req,res)=>{
+    res.sendFile("index.html");
+})
+
 //connection create
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log("Server is running at port 3000");
     
+})
+
+//socket.io connection
+const io = socketio(server); 
+
+//Run when client connects
+io.on("connection", socket =>{
+    //console.log("New connection is connected...");
+
+    //Welcome current user
+    socket.emit('message','Welcome to ChatCord');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit("message","A person has joined the chat");
+
+    //Run when a client discount
+    socket.on('disconnect',()=>{
+        io.emit("message","A person has just left the chat");
+    })
 })
